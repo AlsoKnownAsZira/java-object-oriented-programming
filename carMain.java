5aa3a30392e9d0c631bc7633d86139b02db5c407
brilsat <brilyansatria10@gmail.com>
2023-01-21 23:47:44 +0700

    adding scanner and option to input buyer01

--- carMain.java
+++ carMain.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class carMain {
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("ENTER YOUR NAME: ");
+        String buyer01 = sc.nextLine();
        car car01 = new car();
        car01.manufacturer = "TOYOTA";
        car01.series = "AE 86";
