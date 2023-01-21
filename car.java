40afa4555264a76db1266f6fd815dcf2bd91b7cc
brilsat <brilyansatria10@gmail.com>
2023-01-21 23:45:28 +0700

    Adding carDetail method to show buyer the detail about their wanted cars

--- car.java
+++ car.java
@@ -5,4 +5,8 @@ class car{
     String colour;
     int weight;
     final String produced = "JAPAN";
+
+    void carDetail(String buyer){
+        System.out.println("HI "+buyer+" \n THE CAR YOU'RE GOING TO BUY IS: \n "+manufacturer+" "+series+"\nAVAILABLE COLOUR: "+colour+"\n TOP SPEED "+speed+"KMH"+"\nCAR WEIGHT: "+weight+"KG");
+    }
 }
