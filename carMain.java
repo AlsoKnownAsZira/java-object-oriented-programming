87224c725092930aaeba64e5de7a4c8f838c1283
brilsat <brilyansatria10@gmail.com>
2023-01-21 23:36:51 +0700

    creating field manipulation for object car01

--- carMain.java
+++ carMain.java
@@ -1,5 +1,11 @@
 public class carMain {
     public static void main(String[] args) {
-        
-    }
+       car car01 = new car();
+       car01.manufacturer = "TOYOTA";
+       car01.series = "AE 86";
+       car01.speed = 300;
+       car01.colour = "GRAPHITE WHITE";
+       car01.weight = 400;
+       
+        }
 }
